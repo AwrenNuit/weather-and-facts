8ff4227f6bb825f94da1d04a17be0cc8f385a6d0
AwrenNuit <a.rennaker@yahoo.com>
2020-01-27 22:07:43 -0600

    condensed code

--- server/server.js
+++ server/server.js
@@ -2,24 +2,14 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const bodyParser = require('body-parser');
-
-// Route includes
+const PORT = process.env.PORT || 5000;
 const weatherRouter = require('./routes/weatherRouter');
 
-// Body parser middleware
+app.use(express.static('build'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-// Routes
 app.use('/api/weather', weatherRouter);
 
-// Serve static files
-app.use(express.static('build'));
-
-// App Set
-const PORT = process.env.PORT || 5000;
-
-// Listen
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
