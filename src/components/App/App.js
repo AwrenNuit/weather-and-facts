db5be01f5468d43f84755d8046944541a79c21ca
AwrenNuit <a.rennaker@yahoo.com>
2020-01-29 08:35:50 -0600

    styled footer, added Footer component to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Weather from '../Weather/Weather';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <>
       <Weather />
+      <Footer />
     </>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
-
   return(
-    <>
-    <p>Powered by <a href="https://darksky.net/poweredby/">Dark Sky</a>, <a href="history.muffinlabs.com">History@Muffinlabs</a>, and <a href="http://numbersapi.com">NumbersAPI</a></p>
-    </>
+    <footer className="footer">
+      <p className="footer-p">Powered by <a href="https://darksky.net/poweredby/" target="_blank">Dark Sky</a>, <a href="https://history.muffinlabs.com/" target="_blank">History@Muffinlabs</a>, and <a href="http://numbersapi.com/" target="_blank">NumbersAPI</a></p>
+    </footer>
   );
 }
 
