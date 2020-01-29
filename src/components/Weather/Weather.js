0662ce7c17228ede865d05020f130a60a9f8a352
AwrenNuit <a.rennaker@yahoo.com>
2020-01-29 08:11:37 -0600

    added styling - font color, div centering, headers to each box

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -42,23 +42,35 @@ class Weather extends Component{
 
     return(
       <>
-        <p>Summary: {today.summary}</p>
-        <p>High: {this.state.high}</p>
-        <p>Low: {this.state.low}</p>
-        <p>Humidity: {today.humidity * 100}%</p>
-        <p>Wind Speed: {today.windSpeed}</p>
-        <p>Moon Phase: {today.moonPhase * 100}% illuminated</p>
-        {this.state.temp < 35 ? 
-          <p>Dress warm today!</p>
-          :
-          ''
-        }
-        {this.state.temp > 75 ?
-          <p>Dress light today!</p>
-          :
-          ''
-        }
+        <div className="weather-container">
+          <div className="weather-padding">
+            <h3>Today's Forecast</h3>
+            <p>Summary: {today.summary}</p>
+            <p>High: {this.state.high}</p>
+            <p>Low: {this.state.low}</p>
+            <p>Humidity: {today.humidity * 100}%</p>
+            <p>Wind Speed: {today.windSpeed}</p>
+            <p>Moon: {today.moonPhase * 100}% illuminated</p>
+
+            {this.state.temp < 35 ? 
+              <p className="dress-text">~ Dress warm today! ~</p>
+              :
+              ''
+            }
+            {this.state.temp > 75 ?
+              <p className="dress-text">~ Dress light today! ~</p>
+              :
+              ''
+            }
+            {this.state.temp >= 35 && this.state.temp <= 75 ?
+              <p className="dress-text">~ Dress for anything! ~</p>
+              :
+              ''
+            }
+          </div>
+        </div>
         <div className="history-container">
+          <h3 className="history-head">Today in History</h3>
           <p className="history-text">In {history.year}, {history.text}</p>
         </div>
       </>
