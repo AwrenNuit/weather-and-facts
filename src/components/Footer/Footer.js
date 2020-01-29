402fc4fb8b62a4cd2811f16e46f21a7f9997f0f7
AwrenNuit <a.rennaker@yahoo.com>
2020-01-29 08:17:48 -0600

    set up footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+
+  return(
+    <>
+    <p>Powered by <a href="https://darksky.net/poweredby/">Dark Sky</a>, <a href="history.muffinlabs.com">History@Muffinlabs</a>, and <a href="http://numbersapi.com">NumbersAPI</a></p>
+    </>
+  );
+}
+
+export default Footer;
